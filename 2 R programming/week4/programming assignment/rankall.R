1ff5dc012e9816737904aa51b11f30e5f07d6751
roeeorland <roee.orland@gmail.com>
2017-03-29 00:15:22 +0300

    done with programming assignment

--- 2 R programming/week4/programming assignment/rankall.R
+++ 2 R programming/week4/programming assignment/rankall.R
@@ -1,45 +1,121 @@
 rankall<-function(outcome,num="best")
 {
         measures<-read.csv("outcome-of-care-measures.csv", colClasses = "character")
-        
-        if (outcome=="heart attack")
-                
-                outcome_column<-11
-        
-        else
-                if (outcome=="heart failure")
-                        outcome_column<-17
-                else
-                        if (outcome=="pneumonia")
-                                outcome_column<-23
-                        else
-                                stop("invalid outcome")
-                        
+        measures[]
+        list_names<-names(measures)
+        #measures<-unname(measures)
 
-               
         
-                        
-        measures<-measures[measures[,outcome_column]!="Not Available",]
-        measures[,outcome_column]<-as.numeric(measures[,outcome_column])
-        measures<-measures[,c(2,7,outcome_column)]#leaving us with the hospital name, state and outcome
-        split_measures<-split(measures,measures[,2])#splitting by state
-
-        #
-        for (i in 1:length(split_measures))
+        ## is the outcome valid
+        
+        boolean<-FALSE
+        if (outcome!="heart attack" && outcome!="heart failure" && outcome!="pneumonia")
         {
-                state_measures<-as.data.frame(split_measures[i])
-
-
-                for (j in  1:length(split_measures))
-                {
-
-                }
-
-
-
+                stop("invalid outcome")
+        }
         
-         }
+        ## choosing column according to which outcome we're tracking
+        
+        outcome_column<- 0
+        if(outcome=="heart attack")
+        {
+                outcome_column<-11
+        }
+        else
+        {
+                if (outcome=="heart failure")
+                {
+                        outcome_column<-17
+                        
+                }
+                else
+                {
+                        outcome_column<-23
+                        
+                }
+        }
+        
+        
+        
+        
+        measures[,outcome_column]<-as.numeric(measures[,outcome_column])
+        measures<-measures[!is.na(measures[,outcome_column]),]#getting rid of NAs
+        split_measures<-split(measures,measures[,7])#creates a list of lists per state
+        result<-character(2*length(split_measures))
+        state_names<-character(length(split_measures))
+        dim(result)<-c(length(split_measures),2)
+        for(i in 1:length(split_measures))# a loop running on each state list
+        {
+                state_data<-as.data.frame(split_measures[i])
+                state_names[i]<-state_data[1,7]
+                # names(state_data)<-list_names
+                # rownames(state_data)<-NULL
+                ranking_vector<-numeric(length(state_data[,1]))+1
+                
+                #adjusting num. couldn't do it before because "worst" depends on number of hospitals in state
+                if (num=="best")
+                        num<-1
+                else
+                        if (num=="worst")
+                                num<-length(ranking_vector)
+                
+                
+                
+                
+                
+                ##for every hospital we want to know how many hospitals are better
+                for(j in 1:length(ranking_vector))
+                        for(k in 1:length(ranking_vector))
+                                if((state_data[j,outcome_column]>state_data[k,outcome_column])||((state_data[j,outcome_column]==state_data[k,outcome_column])&&(state_data[j,2]>state_data[k,2])))
+                                        ranking_vector[j]<-ranking_vector[j]+1
+                
+                if (num>length(ranking_vector))
+                {
+                        result[i,1]<-"NA"
+                        result[i,2]<-state_data[1,7]
+                  
+                       
+                }
+                        
+                        
+                
+                        
+                else
+                {
+                        t<-0
+                        for(k in 1:length(ranking_vector))
+                        {
+                                if (num==ranking_vector[k])
+                                        t<-k
+                        }
+                        #result[i,]<-c(state_data[1,7],state_data[t,7])
+                        result[i,1]<-state_data[t,2];
+                        result[i,2]<-state_data[1,7]
+                                
+                                
+                }
+                
+                
+                
+                
+                
+                
+                
+        }#end of main list loop
        
+        result_data_frame<-as.data.frame(result)
+        names(result_data_frame)<-c("hospital","state")
+        #rownames(result_data_frame)<-state_names
+        result_data_frame
+        
+        
+        
+        
+        
+        
+        
+        
         
         
 }
+        
