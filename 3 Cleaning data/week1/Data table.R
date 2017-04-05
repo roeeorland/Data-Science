15b05b0e7464a6fa342b2ac6eb5f0ed05d35724c
roeeorland <roee.orland@gmail.com>
2017-04-05 10:42:02 +0300

    finished data.table file

--- 3 Cleaning data/week1/Data table.R
+++ 3 Cleaning data/week1/Data table.R
@@ -31,3 +31,37 @@ DT[c(2,1)]
 DT[c(2,3)]
 DT[c(2,1)]#gives 2nd row, then 1st row
 DT[,mean(w+z), by=y]#analyze by common attribute
+DT[,.N,by=y]# how often each value of y appears
+
+## Keys
+setkey(DT,y)
+DT["a"]
+
+## use keys to merge (like in sql)
+
+DT2<-data.table(x=1:3,y=rep(c("a","b","c"),each=1))
+DT3<-data.table(x=rnorm(3),y=rep(c("a","b","c"),each=1))
+setkey(DT2,y)
+setkey(DT3,y)
+merge(DT2,DT3)
+
+y x.x        x.y
+1: a   1 -2.2917606
+2: b   2 -0.1692266
+3: c   3  0.4537311
+
+## reading files quickly with fread
+
+big_df<-data.frame(x=rnorm(1e6),y=rnorm(1e6))
+f<-tempfile()
+write.table(big_df,file=f,row.names = FALSE,col.names = TRUE,sep="\t",quote = FALSE)
+system.time(fread(f))
+
+Read 1000000 rows and 2 (of 2) columns from 0.035 GB file in 00:00:09
+user  system elapsed 
+7.94    0.03    8.12 
+
+system.time(read.table(f,header=TRUE,sep="\t"))
+
+user  system elapsed 
+20.78    0.20   21.11 
